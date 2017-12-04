43c0167496c47f2ad1d717582445d1a9f507058d
Spencer <spence521@yahoo.com>
2017-12-04 13:27:57 -0700

    Added gitignore

---
+++ BVC_Filters/BVC_Filters/Data.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BVC_Filters
+{
+    public class Data
+    {
+        public Dictionary<string, string> Training_Data { get; private set; }
+        public Dictionary<string, string> Test_Data { get; private set; }
+        public Data(StreamReader r, StreamReader r2)
+        {
+            Training_Data = new Dictionary<string, string>();
+            Test_Data = new Dictionary<string, string>();
+            SetData(r, r2);
+        }
+        public void SetData(StreamReader reader, StreamReader reader_2 = null)
+        {
+            reader.DiscardBufferedData();
+            reader.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+
+                int Sign;
+                Dictionary<int, double> Vector = new Dictionary<int, double>();
+                string[] splitstring = line.Split();
+                if (splitstring.First().First() == '1') { Sign = 1; }
+                else { Sign = -1; }
+                foreach (var item in splitstring)
+                {
+                    if (item.Contains(":"))
+                    {
+                        string[] s = item.Split(':');
+                        Vector.Add(Convert.ToInt32(s[0]), Convert.ToDouble(s[1]));
+                    }
+                }
+                //Training_Data.Add(new Entry(Sign, Vector));
+            }
+            if (reader_2 != null)
+            {
+                reader_2.DiscardBufferedData();
+                reader_2.BaseStream.Seek(0, System.IO.SeekOrigin.Begin);
+                string line2;
+                while ((line2 = reader_2.ReadLine()) != null)
+                {
+                    int Sign;
+                    Dictionary<int, double> Vector = new Dictionary<int, double>();
+                    string[] splitstring = line2.Split();
+                    if (splitstring.First().First() == '1') { Sign = 1; }
+                    else { Sign = -1; }
+                    foreach (var item in splitstring)
+                    {
+                        if (item.Contains(":"))
+                        {
+                            string[] s = item.Split(':');
+                            Vector.Add(Convert.ToInt32(s[0]), Convert.ToDouble(s[1]));
+                        }
+                    }
+                    //Test_Data.Add(new Entry(Sign, Vector));
+                }
+            }
+        }
+    }
+}
--- BVC_Filters/BVC_Filters/Program.cs
+++ BVC_Filters/BVC_Filters/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,20 @@ namespace BVC_Filters
     {
         static void Main(string[] args)
         {
+            StreamReader Train;
+            StreamReader Test;
+            Data data;
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter a file argument.");
+                return;
+            }
+            else if (args.Length > 1) //at least two arguments
+            {
+                Train = File.OpenText(args[0]);
+                Test = File.OpenText(args[1]);
+                data = new Data(Train, Test);
+            }
         }
     }
 }
