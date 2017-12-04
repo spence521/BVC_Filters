36ddcf1c8e95daf80a3a49a9f92c32822051f536
Spencer <spence521@yahoo.com>
2017-12-04 13:09:32 -0700

    initialize project

---
+++ BVC_Filters/BVC_Filters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BVC_Filters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
