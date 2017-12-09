4bd2a8501a385c07a519cd1b33cb3eaeebb0ebe7
Spencer <spence521@yahoo.com>
2017-12-08 17:56:13 -0700

    Uploading Submission

--- Submission/Classes/RandomNoGen.cs
+++ Submission/Classes/RandomNoGen.cs
@@ -10,17 +10,17 @@ namespace BVC_Filters
     {
         private static Random RandomGenerator { get; set; }
 
-        public static UInt64 Int64Gen()
+        public static int Int64Gen()
         {
             if (RandomGenerator == null)
                 RandomGenerator = new Random();
-            var buffer = new byte[sizeof(UInt64)];
+            var buffer = new byte[sizeof(int)];
             RandomGenerator.NextBytes(buffer);
-            return BitConverter.ToUInt64(buffer, 0);
+            return BitConverter.ToInt32(buffer, 0);
         }
 
-        public static List<UInt64> RandomList(long count) {
-            List<UInt64> list = new List<UInt64>();
+        public static List<int> RandomList(long count) {
+            List<int> list = new List<int>();
             while (count-- > 0)
             {
                 list.Add(Int64Gen());
